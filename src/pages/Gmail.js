d5db15373e4d8fc092081ae768de454655419982
Zaid Khan <53516135+kzaidd@users.noreply.github.com>
2021-08-04 18:54:36 +0530

    Add files via upload

---
+++ src/pages/Gmail.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from '../styles/center'
+
+const Gmail = () => {
+    return (
+        <Container>
+            <h1>gmail</h1>
+        </Container>
+    )
+}
+
+export default Gmail
---
+++ src/pages/Images.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Images = () => {
+    return (
+        <div>
+            <h1>images</h1>
+        </div>
+    )
+}
+
+export default Images;
